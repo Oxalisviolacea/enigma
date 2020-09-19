b143243baa753f22cc02946090ad925a7ba38f5d
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-18 19:48:01 -0600

    Create class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
