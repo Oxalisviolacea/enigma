a8115f69569d0ee75fc9405b3ab51a27bca7ac1d
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-20 15:57:29 -0600

    Create method to make the key into a hash

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -8,4 +8,13 @@ class Decrypt
     @key = key
     @date = date
   end
+
+  def key_hash
+  key_str = @key.rjust(5, '0')
+  number_str_array = [key_str[0..1], key_str[1..2],
+  key_str[2..3], key_str[3..4]]
+  number_array = number_str_array.map {|number| number.to_i}
+  letter_array = ("A".."D").to_a
+  Hash[letter_array.zip(number_array)]
+end
 end
--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -14,4 +14,9 @@ class DecryptTest < Minitest::Test
     assert_equal "2715", @decrypt.key
     assert_equal "40895", @decrypt.date
   end
+
+  def test_it_can_make_the_keys_into_a_hash
+    expected = ({"A" => 02, "B" => 27, "C" => 71, "D" => 15})
+    assert_equal expected, @decrypt.key_hash
+  end
 end
