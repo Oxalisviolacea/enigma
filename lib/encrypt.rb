cc6f2d3617a74633088180ccfa0db875f9049385
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-20 14:56:41 -0600

    Create methods to remove and readd the special characters

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,30 +1,33 @@
 require 'date'
 
 class Encrypt
-  attr_reader :input
+  attr_reader :message, :key, :date
 
-  def initialize
-    @input = "The quick brown fox jumps over the lazy dog."
+  def initialize(message, key = self.random_number, date = self.todays_date)
+    @message = message
+    @key = key
+    @date = date
   end
 
   def random_number
-    rand(99999)
+    @key = rand(99999).to_s.rjust(5, '0')
   end
 
   def key_hash
-    rand_str = random_number.to_s.rjust(5, '0')
-    number_str_array = [rand_str[0..1], rand_str[1..2], rand_str[2..3], rand_str[3..4]]
+    key_str = @key.rjust(5, '0')
+    number_str_array = [key_str[0..1], key_str[1..2],
+    key_str[2..3], key_str[3..4]]
     number_array = number_str_array.map {|number| number.to_i}
     letter_array = ("A".."D").to_a
     Hash[letter_array.zip(number_array)]
   end
 
-  def date
-    Date.today.strftime("%m%d%y").to_i
+  def todays_date
+    Date.today.strftime("%m%d%y")
   end
 
   def offset_squared
-    date ** 2
+    date.to_i ** 2
   end
 
   def offset_hash
@@ -41,13 +44,24 @@ class Encrypt
   end
 
   def format_input
-    @input.delete(".").downcase.chars
-    #other characters should be coded as itself
+    @message.downcase.chars
   end
 
   def input_index
     format_input.map.with_index do |letter, index|
       [letter, index, index % 4]
+    end
+  end
+
+  def find_special_chars
+    input_index.find_all do |array|
+      !alphabet.include?(array[0])
+    end
+  end
+
+  def array_without_special_chars
+    input_index.find_all do |array|
+      alphabet.include?(array[0])
     end
   end
 
@@ -77,7 +91,7 @@ class Encrypt
 
   def shift
     shift_array = []
-    input_index.each do |input_array|
+    array_without_special_chars.each do |input_array|
       if input_array[2] == 0
         shift_array << a_shift.fetch(input_array[0])
       elsif input_array[2] == 1
@@ -91,7 +105,13 @@ class Encrypt
     shift_array
   end
 
+  def insert_special_chars
+    find_special_chars.flat_map do |array|
+      shift.insert(array[1], array[0])
+    end
+  end
+
   def join_shift
-    shift.join
+    insert_special_chars.join
   end
 end
