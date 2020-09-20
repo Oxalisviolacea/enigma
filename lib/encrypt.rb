3e277182c6f7da5c4e17057fde4b8cc5d440c767
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-20 01:34:12 -0600

    Move methods from enigma class to encrypt class

---
+++ lib/encrypt.rb
@@ -0,0 +1,97 @@
+require 'date'
+
+class Encrypt
+  attr_reader :input
+
+  def initialize
+    @input = "The quick brown fox jumps over the lazy dog."
+  end
+
+  def random_number
+    rand(99999)
+  end
+
+  def key_hash
+    rand_str = random_number.to_s.rjust(5, '0')
+    number_str_array = [rand_str[0..1], rand_str[1..2], rand_str[2..3], rand_str[3..4]]
+    number_array = number_str_array.map {|number| number.to_i}
+    letter_array = ("A".."D").to_a
+    Hash[letter_array.zip(number_array)]
+  end
+
+  def date
+    Date.today.strftime("%m%d%y").to_i
+  end
+
+  def offset_squared
+    date ** 2
+  end
+
+  def offset_hash
+    number_str_array = offset_squared.to_s[-4..-1].split("")
+    number_array = number_str_array.map {|number| number.to_i}
+    letter_array = ("A".. "D").to_a
+    Hash[letter_array.zip(number_array)]
+  end
+
+  def keys_and_offsets
+    key_hash.merge!(offset_hash) do |key, key_value, offset_value|
+      key_value + offset_value
+    end
+  end
+
+  def format_input
+    @input.delete(".").downcase.chars
+    #other characters should be coded as itself
+  end
+
+  def input_index
+    format_input.map.with_index do |letter, index|
+      [letter, index, index % 4]
+    end
+  end
+
+  def alphabet
+    ("a".."z").to_a << " "
+  end
+
+  def a_shift
+    shift = keys_and_offsets["A"]
+    Hash[alphabet.zip(alphabet.rotate(shift))]
+  end
+
+  def b_shift
+    shift = keys_and_offsets["B"]
+    Hash[alphabet.zip(alphabet.rotate(shift))]
+  end
+
+  def c_shift
+    shift = keys_and_offsets["C"]
+    Hash[alphabet.zip(alphabet.rotate(shift))]
+  end
+
+  def d_shift
+    shift = keys_and_offsets["D"]
+    Hash[alphabet.zip(alphabet.rotate(shift))]
+  end
+
+  def shift
+    shift_array = []
+    input_index.each do |input_array|
+      if input_array[2] == 0
+        shift_array << a_shift.fetch(input_array[0])
+      elsif input_array[2] == 1
+        shift_array << b_shift.fetch(input_array[0])
+      elsif input_array[2] == 2
+        shift_array << c_shift.fetch(input_array[0])
+      elsif input_array[2] == 3
+        shift_array << d_shift.fetch(input_array[0])
+      end
+    end
+    shift_array
+  end
+
+  def join_shift
+    shift.join
+  end
+end
---
+++ test/encrypt_test.rb
@@ -0,0 +1,134 @@
+require './test/test_helper'
+
+class EncryptTest < Minitest::Test
+  def setup
+    @encrypt = Encrypt.new
+  end
+
+  def test_it_exists
+    assert_instance_of Encrypt, @encrypt
+  end
+
+  def test_it_can_make_the_keys_into_a_hash
+    @encrypt.stubs(:random_number).returns(2715)
+
+    expected = ({"A" => 02, "B" => 27, "C" => 71, "D" => 15})
+    assert_equal expected, @encrypt.key_hash
+  end
+
+  def test_it_can_square_the_date
+    @encrypt.stubs(:date).returns(40895)
+
+    assert_equal 1672401025, @encrypt.offset_squared
+  end
+
+  def test_it_can_create_a_hash_of_offsets
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = ({"A" => 1, "B" => 0, "C" => 2, "D" => 5})
+    assert_equal expected, @encrypt.offset_hash
+  end
+
+  def test_it_can_merge_the_keys_and_offsets_hashes
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = ({"A" => 3, "B" => 27, "C" => 73, "D" => 20})
+    assert_equal expected, @encrypt.keys_and_offsets
+  end
+
+  def test_it_can_format_input
+    @encrypt.stubs(:input).returns("The quick brown fox jumps over the lazy dog.")
+    expected = ["t", "h", "e", " ", "q", "u", "i", "c", "k", " ", "b", "r",
+      "o", "w", "n", " ", "f", "o", "x", " ", "j", "u", "m", "p", "s", " ", "o",
+      "v", "e", "r", " ", "t", "h", "e", " ", "l", "a", "z", "y", " ", "d",
+      "o", "g"]
+    assert_equal expected, @encrypt.format_input
+  end
+
+  def test_it_can_find_the_index_of_the_input
+    expected = [["t", 0, 0], ["h", 1, 1], ["e", 2, 2], [" ", 3, 3],
+    ["q", 4, 0], ["u", 5, 1], ["i", 6, 2], ["c", 7, 3], ["k", 8, 0],
+    [" ", 9, 1], ["b", 10, 2], ["r", 11, 3], ["o", 12, 0], ["w", 13, 1],
+    ["n", 14, 2], [" ", 15, 3], ["f", 16, 0], ["o", 17, 1], ["x", 18, 2],
+    [" ", 19, 3], ["j", 20, 0], ["u", 21, 1], ["m", 22, 2], ["p", 23, 3],
+    ["s", 24, 0], [" ", 25, 1], ["o", 26, 2], ["v", 27, 3], ["e", 28, 0],
+    ["r", 29, 1], [" ", 30, 2], ["t", 31, 3], ["h", 32, 0], ["e", 33, 1],
+    [" ", 34, 2], ["l", 35, 3], ["a", 36, 0], ["z", 37, 1], ["y", 38, 2],
+    [" ", 39, 3], ["d", 40, 0], ["o", 41, 1], ["g", 42, 2]]
+
+    assert_equal expected, @encrypt.input_index
+  end
+
+  def test_it_can_make_an_array_with_the_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+                "y", "z", " "]
+    assert_equal expected, @encrypt.alphabet
+  end
+
+  def test_it_can_create_an_A_shift_hash
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = {"a"=>"d", "b"=>"e", "c"=>"f", "d"=>"g", "e"=>"h", "f"=>"i",
+      "g"=>"j", "h"=>"k", "i"=>"l", "j"=>"m", "k"=>"n", "l"=>"o", "m"=>"p",
+      "n"=>"q", "o"=>"r", "p"=>"s", "q"=>"t", "r"=>"u", "s"=>"v", "t"=>"w",
+      "u"=>"x", "v"=>"y", "w"=>"z", "x"=>" ", "y"=>"a", "z"=>"b", " "=>"c"}
+    assert_equal expected, @encrypt.a_shift
+  end
+
+  def test_it_can_create_an_B_shift_hash
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = {"a"=>"a", "b"=>"b", "c"=>"c", "d"=>"d", "e"=>"e", "f"=>"f",
+      "g"=>"g", "h"=>"h", "i"=>"i", "j"=>"j", "k"=>"k", "l"=>"l", "m"=>"m",
+      "n"=>"n", "o"=>"o", "p"=>"p", "q"=>"q", "r"=>"r", "s"=>"s", "t"=>"t",
+      "u"=>"u", "v"=>"v", "w"=>"w", "x"=>"x", "y"=>"y", "z"=>"z", " "=>" "}
+    assert_equal expected, @encrypt.b_shift
+  end
+
+  def test_it_can_create_an_C_shift_hash
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = {"a"=>"t", "b"=>"u", "c"=>"v", "d"=>"w", "e"=>"x", "f"=>"y",
+      "g"=>"z", "h"=>" ", "i"=>"a", "j"=>"b", "k"=>"c", "l"=>"d", "m"=>"e",
+      "n"=>"f", "o"=>"g", "p"=>"h", "q"=>"i", "r"=>"j", "s"=>"k", "t"=>"l",
+      "u"=>"m", "v"=>"n", "w"=>"o", "x"=>"p", "y"=>"q", "z"=>"r", " "=>"s"}
+    assert_equal expected, @encrypt.c_shift
+  end
+
+  def test_it_can_create_an_D_shift_hash
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = {"a"=>"u", "b"=>"v", "c"=>"w", "d"=>"x", "e"=>"y", "f"=>"z",
+      "g"=>" ", "h"=>"a", "i"=>"b", "j"=>"c", "k"=>"d", "l"=>"e", "m"=>"f",
+      "n"=>"g", "o"=>"h", "p"=>"i", "q"=>"j", "r"=>"k", "s"=>"l", "t"=>"m",
+      "u"=>"n", "v"=>"o", "w"=>"p", "x"=>"q", "y"=>"r", "z"=>"s", " "=>"t"}
+    assert_equal expected, @encrypt.d_shift
+  end
+
+  def test_it_can_rotate_the_shift_the_letters
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = ["w", "h", "x", "t", "t", "u", "a", "w", "n", " ", "u", "k",
+      "r", "w", "f", "t", "i", "o", "p", "t", "m", "u", "e", "i", "v", " ",
+      "g", "o", "h", "r", "s", "m", "k", "e", "s", "e", "d", "z", "q", "t",
+      "g", "o", "z"]
+
+    assert_equal expected, @encrypt.shift
+  end
+
+  def test_it_can_join_the_shift
+    @encrypt.stubs(:random_number).returns(2715)
+    @encrypt.stubs(:date).returns(40895)
+
+    expected = "whxttuawn ukrwftioptmueiv gohrsmkesedzqtgoz"
+
+    assert_equal expected, @encrypt.join_shift
+  end
+end
