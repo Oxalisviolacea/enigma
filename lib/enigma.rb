5bc7ea1d223a732bbf7aa6fd53099363de476951
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-21 13:52:38 -0600

    Fix spelling mistakes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,15 +7,25 @@ class Enigma
 
   def encrypt(message, key = @random_key, date = @today)
     encrypt = Encrypt.new(message, key, date)
-    {encryption: "#{encrypt.ciphertext.dup}",
-    key: "#{encrypt.key}",
-    date: "#{encrypt.date}"}
+    encrypt_hash = Hash.new(0)
+    encrypt_hash[:encryption] = encrypt.ciphertext
+    encrypt_hash[:key] = key
+    encrypt_hash[:date] = date
+    encrypt_hash
+    # {encryption: encrypt.ciphertext,
+    # key: encrypt.key,
+    # date: encrypt.date}
   end
 
   def decrypt(ciphertext, key, date = @today)
     decrypt = Decrypt.new(ciphertext, key, date)
-    {decryption: "#{decrypt.message}",
-    key: "#{decrypt.key}",
-    date: "#{decrypt.date}"}
+    decrypt_hash = Hash.new(0)
+    decrypt_hash[:decryption] = decrypt.message
+    decrypt_hash[:key] = key
+    decrypt_hash[:date] = date
+    decrypt_hash
+    # {decryption: decrypt.message,
+    # key: decrypt.key,
+    # date: decrypt.date}
   end
 end
