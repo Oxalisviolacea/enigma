52cd3ece6e0981282920e0294d499e261405a2fd
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-20 22:43:48 -0600

    Modify encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,25 +1,21 @@
+require './lib/encrypt'
+
 class Enigma
   attr_reader :message, :ciphertext
   def initialize
-    @message = message
-    @ciphertext = ciphertext
   end
 
   def encrypt(message, key, date)
-    Encrypt.new(message, key, date)
-    encrypt_hash = {
-    encryption: "#{encrypt.ciphertext}",
-    key: "#{encypt.key}",
-    date: "#{encrypt.date}"
-    }
+    encrypt = Encrypt.new(message, key, date)
+    {encryption: "#{encrypt.ciphertext}",
+    key: "#{encrypt.key}",
+    date: "#{encrypt.date}"}
   end
 
   def decrypt(ciphertext, key, date)
-    Decrypt.new(ciphertext, key, date)
-    decrypt_output = {
-    encryption: "#{encrypt.message}",
-    key: "#{encypt.key}",
-    date: "#{encrypt.date}"
-    }
+    decrypt = Decrypt.new(ciphertext, key, date)
+    {decryption: "#{decrypt.message}",
+    key: "#{decrypt.key}",
+    date: "#{decrypt.date}"}
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,11 +9,19 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_can_read_the_message_file
-    assert_equal "The quick brown fox jumps over the lazy dog.", @enigma.message
+  def test_it_can_encrypt_a_message
+    expected = {encryption: "whxttuawn ukrwftioptmueiv gohrsmkesedzqtgoz.",
+                key: "2715",
+                date: "40895"}
+    assert_equal expected, @enigma.encrypt(
+      "The quick brown fox jumps over the lazy dog.", "2715", "40895")
   end
 
-  def test_it_can_write_to_the_encrpted_file
-    assert_equal "whxttuawn ukrwftioptmueiv gohrsmkesedzqtgoz", @enigma.ciphertext
+  def test_it_can_decrypt_a_message
+    expected = {decryption: "The quick brown fox jumps over the lazy dog.",
+                key: "2715",
+                date: "40895"}
+    assert_equal expected, @enigma.decrypt(
+      "whxttuawn ukrwftioptmueiv gohrsmkesedzqtgoz.", "2715", "40895")
   end
 end
