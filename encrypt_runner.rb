acc24d4ba581b22ac0ff78661faac00c6b4f5d8b
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-20 22:45:30 -0600

    WIP hardcoded some things

--- encrypt_runner.rb
+++ encrypt_runner.rb
@@ -7,20 +7,8 @@ message = handle.read
 handle.close
 
 enigma = Enigma.new
-ciphertext = enigma.encrypt(message, "02715", "040895")
+ciphertext = enigma.encrypt("The quick brown fox jumps over the lazy dog.", "2715", "40895")
 puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.date}"
 
 writer = File.open(ARGV[1], "w")
 writer.write(ciphertext)
-
-
-
-# #this will return the hash with the ecrypted message
-# enigma.decrypt("keder ohulw", "02715", "040895")
-# #this will return the hash with the decypted message
-# encrypted = enigma.encrypt("hello world", "02715")
-# #returns hash with default as todays date
-# enigma.decrypt(encrypted[:encryption], "02715")
-# #returns hash with default as todays date
-# enigma.encrypt("hello world")
-# #returns hash with random key and default as todays date
