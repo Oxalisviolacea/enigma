ec6ecb58f5e6b2dd4915e1bb5f3198d38739b9d4
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-21 13:53:31 -0600

    Change user output

--- encrypt_runner.rb
+++ encrypt_runner.rb
@@ -4,13 +4,13 @@ require './lib/encrypt'
 
 handle = File.open(ARGV[0], "r")
 incoming_text = handle.read
-
 message = incoming_text.chomp
 handle.close
+
 enigma = Enigma.new
 ciphertext = enigma.encrypt(message, "2715", "40895")
-puts "Created #{ARGV[1]} with the key #{ciphertext[:key]} and date #{ciphertext[:date]}"
 
 writer = File.open(ARGV[1], "w")
 writer.write(ciphertext)
-# puts ciphertext[:encryption].chomp
+
+puts "Created #{ARGV[1]} with the key #{ciphertext[:key]} and date #{ciphertext[:date]}"
