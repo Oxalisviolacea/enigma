6cb23189ba31953ff33d9f26e8eb0f2513f3b5b1
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-21 19:11:43 -0600

    Refactor offsetable module

--- lib/offsetable.rb
+++ lib/offsetable.rb
@@ -3,19 +3,21 @@ module Offsetable
     @date.to_i ** 2
   end
   
+  def letter_array
+    letter_array = ("A".."D").to_a
+  end
+
   def key_hash
     key_str = @key.rjust(5, '0')
     number_str_array = [key_str[0..1], key_str[1..2],
     key_str[2..3], key_str[3..4]]
     number_array = number_str_array.map {|number| number.to_i}
-    letter_array = ("A".."D").to_a
     Hash[letter_array.zip(number_array)]
   end
 
   def offset_hash
     number_str_array = offset_squared.to_s[-4..-1].split("")
     number_array = number_str_array.map {|number| number.to_i}
-    letter_array = ("A".. "D").to_a
     Hash[letter_array.zip(number_array)]
   end
 
