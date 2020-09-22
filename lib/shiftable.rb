e574a49f5b30c4f6373a9c52ba0d7e207ed4dd55
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-21 19:13:25 -0600

    Refactor shift method

--- lib/shiftable.rb
+++ lib/shiftable.rb
@@ -6,18 +6,16 @@ module Shiftable
   end
 
   def shift
-    shift_array = []
-    array_without_special_chars.each do |input_array|
+    array_without_special_chars.map do |input_array|
       if input_array[2] == 0
-        shift_array << a_shift.fetch(input_array[0])
+        a_shift.fetch(input_array[0])
       elsif input_array[2] == 1
-        shift_array << b_shift.fetch(input_array[0])
+        b_shift.fetch(input_array[0])
       elsif input_array[2] == 2
-        shift_array << c_shift.fetch(input_array[0])
+        c_shift.fetch(input_array[0])
       elsif input_array[2] == 3
-        shift_array << d_shift.fetch(input_array[0])
+        d_shift.fetch(input_array[0])
       end
     end
-    shift_array
   end
 end
